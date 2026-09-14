c1baa4e25189d390bc7feb71d2e40b58e048f13b
Julien Bingolles <julien.bignolles@ens.uvsq.fr>
2026-09-14 15:55:45 +0200

    Read_line et unwrap

--- src/devine_mon_nombre.rs
+++ src/devine_mon_nombre.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("Hello World!");
+    println!("Devine mon nombre !\n");
+    println!("Saisissez votre proposition.");
+
+    let mut proposition = String::new();
+    io::stdin().read_line(&mut proposition).unwrap();
+
+    println!("Vous avez saisi : {}", proposition);
 }
